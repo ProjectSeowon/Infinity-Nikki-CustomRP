7d34ebcf06e6850e18e0e7b60a42dacb29d5757d
ProjectSeowon <projectyuki@naver.com>
2024-12-05 16:31:40 -0500

    Added files to show locations

---
+++ Program Files/InfinityNikkiGlobal Launcher/NikkiRP/Nikki-Close.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+taskkill /F /IM "xstarter.exe"
+taskkill /F /IM "CustomRP.exe" 
+ECHO "closed"
---
+++ Program Files/InfinityNikkiGlobal Launcher/NikkiRP/Nikki-Launch.bat
@@ -0,0 +1,3 @@
+@ECHO ON
+START "launcher.exe" "C:\Program Files\InfinityNikkiGlobal Launcher\launcher.exe"
+START "Nikki.crp" "C:\Program Files\InfinityNikkiGlobal Launcher\NikkiRP\Nikki.crp"
